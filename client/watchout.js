eb9d15b9c017d616367bce58aff0c78d33435a1b
DanDai <ddai85@gmail.com>
2017-09-05 14:19:07 -0700

    Dots transition from one position to another

--- client/watchout.js
+++ client/watchout.js
@@ -2,7 +2,7 @@
 var gameOptions = {
   height: 300,
   width: 500,
-  nEnemies: 30,
+  nEnemies: 10,
   padding: 20
 }
 
@@ -12,34 +12,55 @@ var gameBoard = d3.select('.board').append('svg:svg')
                 .attr('height', gameOptions.height)
 
 
-var createEnemies = function(){
+var findEnemyPos = function(){
   return _.range(0,gameOptions.nEnemies).map(function(i){ 
     return {
       id: i,
-      x: Math.random()*100,
-      y: Math.random()*100
+      x: Math.random()*gameOptions.width,
+      y: Math.random()*gameOptions.height
     }
   });
 }
 
-var enemy_data = createEnemies();
+var enemy_data = findEnemyPos();
 
+var moveAround = function() {
+	var obj = {}
+	  obj.x = Math.random()*gameOptions.width;
+    obj.y = Math.random()*gameOptions.height;
 
+	return obj;
 
-var render = function() {
-	var enemies = gameBoard.selectAll("enemy").data(enemy_data, function(d){return d.id});
+}
+
+var render = function(enemy_data) {
+	var enemies = gameBoard.selectAll('enemy').data(enemy_data, function(d){return d.id});
 
 	enemies.enter()
 		.append('svg:circle')
 		.attr('class', 'enemy')
-		.attr('cx', function(d){return d.x * gameOptions.width / 100})
-		.attr('cy', function(d){return d.y * gameOptions.height / 100})
+		.attr('cx', function(d){return d.x})
+		.attr('cy', function(d){return d.y})
 		.attr('r', 10)
 		.attr('fill', 'purple')
 
+  setInterval(function(){
+	  enemies
+	    .transition()
+	    .duration(1000)
+	    .attr('cx', function(d) {var obj = moveAround(); return obj.x})
+	    .attr('cy', function(d) {var obj = moveAround(); return obj.y})
+  }, 1000);
 }
 
-render();
+render(enemy_data);
+
+
+
+// setInterval(function(){
+// 	newEnemyData = findEnemyPos();
+// 	render(newEnemyData);
+// }, 1000)
 
 // var play = function() {
   
